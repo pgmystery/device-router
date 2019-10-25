177c0c9c8f1381991a3dd473648a288867509661
Phil <>
2019-10-25 11:05:27 +0200

    Initial Buttons storybook

--- frontend/src/components/utils/Button.stories.js
+++ frontend/src/components/utils/Button.stories.js
@@ -1,16 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import { withKnobs, text, boolean } from '@storybook/addon-knobs'
 import { withInfo } from '@storybook/addon-info';
-import Button from './Button'
+import { Button, ButtonPrimary, ButtonDanger, ButtonSuccess } from './Button'
 
 
 export default {
-  title: 'Button',
-  decorators: [withKnobs, withInfo]
+  title: 'Buttons',
+  decorators: [withKnobs, withInfo, Wrapper]
 }
 
 export const standard = () =>
-  <Button disabled={boolean('Disabled', false)}>{text('text', 'CLICK ME')}</Button>
+  <Button disabled={boolean('Disabled', false)} tooltip={text('tooltip', 'Tooltip')}>{text('text', 'CLICK ME')}</Button>
+
+export const primary = () =>
+<ButtonPrimary disabled={boolean('Disabled', false)} tooltip={text('tooltip', 'Tooltip')}>{text('text', 'CLICK ME')}</ButtonPrimary>
+
+export const danger = () =>
+  <ButtonDanger disabled={boolean('Disabled', false)} tooltip={text('tooltip', 'Tooltip')}>{text('text', 'CLICK ME')}</ButtonDanger>
+
+export const success = () =>
+<ButtonSuccess disabled={boolean('Disabled', false)} tooltip={text('tooltip', 'Tooltip')}>{text('text', 'CLICK ME')}</ButtonSuccess>
+
+const Wrapper = styled.div`
+  padding: 50px;
+`
+
 
 standard.story = {
   name: 'Default'
